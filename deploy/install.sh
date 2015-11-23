8333dc24667039dcb297b3ae25c66fe8ff224838
Eugene Gritskevich <qwaker.00@gmail.com>
2015-11-23 02:30:30 +0000

    Make link to nginx config instead copy

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,5 +1,5 @@
 sudo mkdir /usr/share/kittenmash/
 sudo ln -s "$(pwd)"/../www /usr/share/kittenmash/
 sudo ln -s "$(pwd)"/../build/ /usr/lib/kittenmash
-sudo cp ./nginx.conf /etc/nginx/sites-enabled/kittenmash.conf
+sudo ln -s "$(pwd)"/nginx.conf /etc/nginx/sites-enabled/kittenmash.conf
 sudo nginx -s reload
