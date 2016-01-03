530614c5c2f3637e1b77a5a1e672653eddc86e6e
Eugene Gritskevich <qwaker.00@gmail.com>
2016-01-03 23:41:43 +0000

    Make ammo closer to real: create vote / get vote / set result

--- load/create_ammo.py
+++ load/create_ammo.py
@@ -57,13 +57,47 @@ def make_all_ammo(ammo):
 
         output(make_ammo(method, url, headers, case, body))
 
+random.seed(666)
+
+db.votes.remove()
+db.images.update_many({}, {'$set' : {'gameCount' : 0l, 'rating' : 1200.0}})
+ammo_post, ammo_put, ammo_get = [], [], []
+for item in xrange(80000):
+    vote = '%024x' % random.randint(0, 2 ** (8 * 12) - 1)
+    ammo_post.append( ('POST', '/vote/%s' % vote, 'create_vote', '') )
+    ammo_get.append( ('GET', '/vote/%s' % vote, 'get_vote', '') )
+    ammo_put.append( ('PUT', '/vote/%s/result' % vote, 'set_vote', 'left' if random.randint(0, 1) == 0 else 'right') )
+
 ammo = []
-for item in db.votes.find():
-    left, right, vote = item['leftId'], item['rightId'], item['_id']
-    ammo.append( ('GET', '/vote/%s' % vote, 'get_vote', '') )
-    ammo.append( ('GET', '/image/%s' % left, 'get_image', '') )
-    ammo.append( ('GET', '/image/%s' % right, 'get_image', '') )
-    ammo.append( ('PUT', '/vote/%s/result' % vote, 'set_vote', 'left' if random.randint(0, 1) == 0 else 'right') )
+deficit1 = 0
+deficit2 = 0
+deficit_thres = 2000
+while len(ammo_post) + len(ammo_put) + len(ammo_get) > 0:
+    if len(ammo_put) + len(ammo_get) > 0 and (
+            len(ammo_post) == 0 or
+            (deficit1 > deficit_thres and random.randint(1, deficit1 - deficit_thres) != 1)
+    ):
+        if len(ammo_put) > 0 and (
+                len(ammo_get) == 0 or
+                (deficit2 > deficit_thres and random.randint(1, deficit2 - deficit_thres) != 1)
+        ):
+            ammo.append( ammo_put[-1] )
+            del ammo_put[-1]
+
+            deficit2 -= 1
+        else:
+            ammo.append( ammo_get[-1] )
+            del ammo_get[-1]
+
+            deficit2 += 1
+
+        deficit1 -= 1
+    else:
+        ammo.append( ammo_post[-1] )
+        del ammo_post[-1]
+
+        deficit1 += 1
+
     if random.randint(1, 20) == 1:
         ammo.append( ('GET', '/top', 'get_top', '') )
 
