292cd7373b46756b7bb5fd5d9bdbbd7c3932cb17
Eugene Gritskevich <qwaker.00@gmail.com>
2016-01-05 14:22:30 +0000

    More waiting time for updater

--- src/updater/main.cpp
+++ src/updater/main.cpp
@@ -22,7 +22,7 @@ int main() {
 
         if (votes.size() == 0) {
             if (batchSize <= 10) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             } else {
                 batchSize /= 2;
             }
